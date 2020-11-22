fe3ae76d80997444af1a6cd852fcca1841ad1cdf
icecolla <icecoladrink@gmail.com>
2020-11-23 00:02:24 +0300

    Added Scripts

---
+++ Editor/TerrainBuilderEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(generateTerrain))]
+public class TerrainBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        generateTerrain myScript = (generateTerrain)target;
+
+        if (GUILayout.Button("Generate Terrain"))
+        {
+            myScript.Start();
+        }
+    }
+}
+
+[CustomEditor(typeof(paintTerrain))]
+public class TerrainPaintEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        paintTerrain myScript = (paintTerrain)target;
+
+        if (GUILayout.Button("Paint Terrain"))
+        {
+            myScript.Start();
+        }
+    }
+}
